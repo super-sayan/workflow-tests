2095a924726ff623b092906554d0b6ff82596fb6
Savelii Sh <shco@list.ru>
2024-03-20 15:34:23 +0200

    Delete react-multi-page-website directory

--- react-multi-page-website/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as Menu } from "./Menu";
-export { default as Contact } from "./Contact";
-export { default as Appointments } from "./Appointments/Appointments";
-export { default as Posts } from "./Appointments/Posts";
-export { default as Post } from "./Appointments/Post";
